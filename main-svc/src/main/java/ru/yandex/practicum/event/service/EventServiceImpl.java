0ca13692a9a5f27251e2346274b3dba0dfb7197d
arak <avrak@yandex.ru>
2025-08-25 07:20:37 +0300

    ExploreWithMe.main-svc: доработки для github

--- main-svc/src/main/java/ru/yandex/practicum/event/service/EventServiceImpl.java
+++ main-svc/src/main/java/ru/yandex/practicum/event/service/EventServiceImpl.java
@@ -369,14 +369,9 @@ public class EventServiceImpl implements EventService {
             throw new NotFoundException("Event with id=" + eventId + " was not published");
         }
 
-
-//        if (viewsBefore < getViewsFromStats(uri)) {
-//            event.setViews(event.getViews() + 1);
-//            eventRepository.save(event);
-//        }
-
         if (viewsBefore < 1) {
             event.setViews(event.getViews() + 1);
+            eventRepository.save(event);
         }
 
         EventFullDto fullDto = EventMapper.toFullDto(event);
